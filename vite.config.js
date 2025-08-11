8727915fe1d4331e6fb60d7c4dbc2361927148b8
Johnyyy1 <jonas.sobotka@gmail.com>
2025-08-11 21:45:37 +0200

    Update build configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Crypto-Dashboard-Website/'
+  base: '/Crypto-Dashboard-Website/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      }
+    }
+  }
 })
